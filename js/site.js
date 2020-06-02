4c30dfb527c60dacc0fece81d9785a6380fc7ffa
Olamide <aboyejiolamide15@gmail.com>
2020-06-02 01:55:49 +0100

    major updates. v2

--- js/site.js
+++ js/site.js
@@ -46,115 +46,42 @@ const name = ()=>toPosition(myName);
 const title = ()=>toPosition(myTitle);
 window.onload = setTimeout(name, 1000), setTimeout(title, 3000), setTimeout(socials, 5000);
 
-//particles.js
-particlesJS("particles-js",{
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#000000"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#567"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#e4e5e3",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
 
+//Dark mode
+const darkBtn = document.querySelector('.dark-btn');
+const lightBtn = document.querySelector('.light-btn');
+
+const showModeButton = mode => {
+  switch (mode) {
+    case 'dark':
+      darkBtn.style.display = 'block';
+      lightBtn.style.display = 'none';
+      break;
+  
+    default:
+      darkBtn.style.display = 'none';
+      lightBtn.style.display = 'block';
+      break;
+  }
+}
+
+let isDark = window.matchMedia('(prefers-color-scheme : dark)').matches;
+let currentMode = isDark ? 'dark' : 'light';
+showModeButton(currentMode);
+document.documentElement.setAttribute('data-theme', currentMode);
+
+const switchMode = () => {
+  if(currentMode === 'light') {
+    currentMode = 'dark';
+    document.documentElement.setAttribute('data-theme', 'dark');
+    showModeButton('dark');
+  }
+  else {
+    currentMode = 'light';
+    document.documentElement.setAttribute('data-theme', 'light');
+    showModeButton('light');
+  }
+}
+
+darkBtn.addEventListener('click', switchMode);
+lightBtn.addEventListener('click', switchMode);
